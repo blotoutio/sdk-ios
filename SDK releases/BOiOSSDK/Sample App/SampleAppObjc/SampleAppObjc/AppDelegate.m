47341413ed1babc5783cf9a7234575ac2ab49017
blotouta2 <ankur@blotout.io>
2021-03-09 17:03:30 +0530

    bump version to 0.6.0

---
+++ SDK releases/BOiOSSDK/Sample App/SampleAppObjc/SampleAppObjc/AppDelegate.m
@@ -0,0 +1,50 @@
+//
+//  AppDelegate.m
+//  SampleAppObjc
+//
+//  Created by Shefali Shrivastava on 26/03/20.
+//  Copyright © 2020 Blotout. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+//#import "BlotoutAnalytics.h"
+//Or
+//#import "BlotoutSDK/BlotoutAnalytics.h"
+#import "BlotoutAnalytics.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // Override point for customization after application launch.
+    BlotoutAnalytics *boaObj = [BlotoutAnalytics sharedInstance];
+    [boaObj initializeAnalyticsEngineUsingKey:@"5DNGP7DR2KD9JSY" url:@"http://dev.blotout.io" andCompletionHandler:^(BOOL isSuccess, NSError * _Nullable error) {
+        [boaObj logEvent:@"AppLaunched" withInformation:launchOptions];
+    }];
+    return YES;
+}
+
+
+#pragma mark - UISceneSession lifecycle
+
+
+- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
+    // Called when a new scene session is being created.
+    // Use this method to select a configuration to create the new scene with.
+    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
+}
+
+
+- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
+    // Called when the user discards a scene session.
+    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+}
+
+
+@end
